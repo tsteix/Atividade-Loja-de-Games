37477e3eb54d252f328024998bc729e911ec6fa7
Tiago <tiagosantos.t11@gmail.com>
2022-01-25 10:28:24 -0300

    Etapa 1 - Loja de games

---
+++ MinhaLojaDeGames/src/main/java/br/com/generation/MinhaLojaDeGames/MinhaLojaDeGamesApplication.java
@@ -0,0 +1,13 @@
+package br.com.generation.MinhaLojaDeGames;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MinhaLojaDeGamesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MinhaLojaDeGamesApplication.class, args);
+	}
+
+}
---
+++ MinhaLojaDeGames/src/test/java/br/com/generation/MinhaLojaDeGames/MinhaLojaDeGamesApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.generation.MinhaLojaDeGames;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MinhaLojaDeGamesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
